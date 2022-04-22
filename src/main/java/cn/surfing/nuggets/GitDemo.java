db7b0566591f4371bd380caa05c6bc4d74474449
zouzc <690424580@qq.com>
2022-04-22 16:27:44 +0800

    gitdemo编写了一个类

---
+++ src/main/java/cn/surfing/nuggets/GitDemo.java
@@ -0,0 +1,12 @@
+package cn.surfing.nuggets;
+
+/**
+ * @Author: zouzc
+ * @Time: 2022/4/22 16:25
+ * @Description:
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
