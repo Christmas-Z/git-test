d85aebae0302012e38c8d8ae9ff74bf8178f347c
zouzc <690424580@qq.com>
2022-04-22 16:29:01 +0800

    gitdemo修改为hacker

--- src/main/java/cn/surfing/nuggets/GitDemo.java
+++ src/main/java/cn/surfing/nuggets/GitDemo.java
@@ -7,6 +7,7 @@ package cn.surfing.nuggets;
  */
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+//        System.out.println("hello world!");
+        System.out.println(" i am a hacker");
     }
 }
